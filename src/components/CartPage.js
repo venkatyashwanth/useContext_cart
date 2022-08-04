e162fd11fcd4be20a72d30e08571a24552436f04
venkat yashwanth <mudiliyashwanth@gmail.com>
2022-08-04 19:02:18 +0530

    data

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ButtonContainer from './ButtonContainer';
-
+import React from "react";
+import ButtonContainer from "./ButtonContainer";
+import './CartPage.css'
 const products = [
   {
     id: 1,
-    product: 'Mouse',
+    product: "Mouse",
   },
   {
     id: 2,
-    product: 'Keyboard',
+    product: "Keyboard",
   },
 ];
 
@@ -16,12 +16,14 @@ const CartPage = () => {
   return (
     <>
       <h1>Items to buy</h1>
-      {products.map((item) => (
-        <div key={item.id}>
-          <p>{item.product}</p>
-          <ButtonContainer />
-        </div>
-      ))}
+      <div className="productContainer">
+        {products.map((item) => (
+          <div key={item.id} className="products">
+            <p>{item.product}</p>
+            <ButtonContainer />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
--- src/components/NavContainer.js
+++ src/components/NavContainer.js
@@ -1,11 +1,11 @@
-
+import './NavContainer.css'
 import { useShare } from './Sharecontext';
 
 const NavContainer = () => {
 const count = useShare();
   return (
     <nav>
-      <ul>
+      <ul className='listContainer'>
         <li>Cart: {count}</li>
         <li>Link 2</li>
       </ul>
